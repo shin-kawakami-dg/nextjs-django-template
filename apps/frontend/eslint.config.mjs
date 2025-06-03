2aeeb2da310705357435d25a24b159047ce3a33f
shin-kawakami <shin-kawakami@garage.co.jp>
2025-06-03 12:57:13 +0900

    Setup prettier and eslint

--- apps/frontend/eslint.config.mjs
+++ apps/frontend/eslint.config.mjs
@@ -1,6 +1,12 @@
+/**
+ * [TODO] eslint-plugin-tailwindcss will be added when tailwindcssv4 is supported
+ * https://github.com/francoismassart/eslint-plugin-tailwindcss/pull/385
+ */
+
+import { FlatCompat } from "@eslint/eslintrc";
+import eslintConfigPrettier from "eslint-config-prettier";
 import { dirname } from "path";
 import { fileURLToPath } from "url";
-import { FlatCompat } from "@eslint/eslintrc";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
@@ -11,6 +17,17 @@ const compat = new FlatCompat({
 
 const eslintConfig = [
   ...compat.extends("next/core-web-vitals", "next/typescript"),
+  {
+    rules: {
+      "react/react-in-jsx-scope": "off",
+      "react/self-closing-comp": "error",
+      "react/jsx-curly-brace-presence": "error",
+      "object-shorthand": "error",
+      "no-unused-vars": ["warn", { argsIgnorePattern: "^_" }],
+      "no-console": ["warn", { allow: ["warn", "error"] }],
+    },
+  },
+  eslintConfigPrettier,
 ];
 
 export default eslintConfig;
